98a00fbae0ecb52a991e21df39e5eed22f6d5eb7
justine <none@none>
2014-06-13 11:47:54 -0600

    Added Storage project with SqlBlobProvider.

---
+++ src/Spring2.Common.Storage/Blob/AzureBlobProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spring2.Common.Storage.Blob {
+    public class AzureBlobProvider : IBlobProvider {
+
+	#region IBlobProvider Members
+
+	public byte[] GetByteArray(string key) {
+	    throw new NotImplementedException();
+	}
+
+	public void Store(string key, byte[] data) {
+	    throw new NotImplementedException();
+	}
+
+	#endregion
+    }
+}
---
+++ src/Spring2.Common.Storage/Blob/SqlBlobProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spring2.Common.Storage.Blob {
+    public class SqlBlobProvider : IBlobProvider {
+
+	public string ConnectionString { get; private set; }
+	public string BlobTable { get; private set; }
+	public string KeyColumn { get; private set; }
+	public string DataColumn { get; private set; }
+
+	public SqlBlobProvider(String connectionString, string blobTable, string keyColumn, string dataColumn) {
+	    this.ConnectionString = connectionString;
+	    this.BlobTable = blobTable;
+	    this.KeyColumn = keyColumn;
+	    this.DataColumn = dataColumn;
+	}
+
+	#region IBlobProvider Members
+
+	public byte[] GetByteArray(string key) {
+	    using (var conn = new SqlConnection(ConnectionString)) {
+		var qry = String.Format("select [{0}] from [{1}] where [{2}] = @key", DataColumn, BlobTable, KeyColumn);
+		var cmd = new SqlCommand(qry, conn);
+		cmd.Parameters.AddWithValue("key", key);		
+		conn.Open();
+		var obj = cmd.ExecuteScalar();
+		if (obj != null) {
+		    return (byte[])obj;
+		}
+		return null;
+	    }
+	}
+
+	public void Store(string key, byte[] data) {
+	    using (var conn = new SqlConnection(ConnectionString)) {
+		var qry = String.Format("if not exists (select 1 from [{0}] where [{1}] = @key) insert into [{0}] ([{1}], [{2}]) values (@key, @data) else update [{0}] set [{2}] = @data where [{1}] = @key",
+		    BlobTable, KeyColumn, DataColumn
+		);
+		
+		var cmd = new SqlCommand(qry, conn);
+		cmd.Parameters.AddWithValue("key", key);
+		cmd.Parameters.AddWithValue("data", data);
+		conn.Open();
+		cmd.ExecuteNonQuery();
+	    }
+	}
+
+	#endregion
+    }
+}
