70a5459b41ff323a8990fe136feaaf2f2d5071ab
joes@spring2.com <joes@spring2.com>
2016-12-01 00:35:58 -0700

    more fully populate build script

--- src/Build.ps1
+++ src/Build.ps1
@@ -2,6 +2,11 @@ echo "build: Build started"
 
 Push-Location $PSScriptRoot
 
+if(Test-Path .\artifacts) {
+	echo "build: Cleaning .\artifacts"
+	Remove-Item .\artifacts -Force -Recurse
+}
+
 & dotnet restore --no-cache
 
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
@@ -17,6 +22,28 @@ foreach ($src in ls src/*) {
 
     & dotnet pack -c Release -o ..\..\artifacts --version-suffix=$suffix
     if($LASTEXITCODE -ne 0) { exit 1 }    
+
+    Pop-Location
+}
+
+foreach ($test in ls test/*.PerformanceTests) {
+    Push-Location $test
+
+	echo "build: Building performance test project in $test"
+
+    & dotnet build -c Release
+    if($LASTEXITCODE -ne 0) { exit 2 }
+
+    Pop-Location
+}
+
+foreach ($test in ls test/*.Tests) {
+    Push-Location $test
+
+	echo "build: Testing project in $test"
+
+    & dotnet test -c Release
+    if($LASTEXITCODE -ne 0) { exit 3 }
 
     Pop-Location
 }
