2f4f3aa9727a72b3ffd7e9eca500dea08552137a
Rui Fang Li <ruifang@spring2.com>
2017-02-06 09:27:00 -0700

    use configured json settings when writing output in exception handlers

--- src/Spring2.Common.Web.Mvc/GenericExceptionMiddleware.cs
+++ src/Spring2.Common.Web.Mvc/GenericExceptionMiddleware.cs
@@ -1,36 +1,35 @@
-using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Spring2.Common.Web.Mvc
-{
-    public class GenericExceptionMiddleware
-    {
-        private readonly RequestDelegate next;
+namespace Spring2.Common.Web.Mvc {
+    public class GenericExceptionMiddleware {
+	private readonly RequestDelegate next;
+	private readonly IOptions<MvcJsonOptions> options;
 
-        // example: https://github.com/aspnet/Diagnostics/blob/dev/src/Microsoft.AspNetCore.Diagnostics/DeveloperExceptionPage/DeveloperExceptionPageMiddleware.cs#L69-L106
-        public GenericExceptionMiddleware(RequestDelegate next)
-        {
-            this.next = next;
-        }
+	public GenericExceptionMiddleware(RequestDelegate next, IOptions<MvcJsonOptions> options) {
+	    this.next = next;
+	    this.options = options;
+	}
 
-        public async Task Invoke(HttpContext context)
-        {
-            try
-            {
-                await this.next.Invoke(context);
-            }
-            catch (Exception exception)
-            {
-                context.Response.StatusCode = 500;
-                context.Response.ContentType = "application/json";
-                string message = JsonConvert.SerializeObject(exception, new JsonSerializerSettings()
-                {
-                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                });
-                await context.Response.WriteAsync(message);
-            }
-        }
+	public async Task Invoke(HttpContext context) {
+	    try {
+		await this.next.Invoke(context);
+	    } catch (Exception exception) {
+
+		context.Response.StatusCode = 500;
+		context.Response.ContentType = "application/json";
+
+
+		string message = JsonConvert.SerializeObject(exception, options.Value.SerializerSettings);
+		await context.Response.WriteAsync(message);
+	    }
+	}
     }
 }
--- src/Spring2.Common.Web.Mvc/HttpExceptionMiddleware.cs
+++ src/Spring2.Common.Web.Mvc/HttpExceptionMiddleware.cs
@@ -1,43 +1,37 @@
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
-namespace Spring2.Common.Web.Mvc
-{
+namespace Spring2.Common.Web.Mvc {
 
-    public class HttpExceptionMiddleware
-    {
-        private readonly RequestDelegate next;
+    public class HttpExceptionMiddleware {
+	private readonly RequestDelegate next;
+	private readonly IOptions<MvcJsonOptions> options;
 
-        public HttpExceptionMiddleware(RequestDelegate next)
-        {
-            this.next = next;
-        }
+	public HttpExceptionMiddleware(RequestDelegate next, IOptions<MvcJsonOptions> options) {
+	    this.next = next;
+	    this.options = options;
+	}
 
-        public async Task Invoke(HttpContext context)
-        {
-            try
-            {
-                await this.next.Invoke(context);
-            }
-            catch (HttpException httpException)
-            {
-                context.Response.StatusCode = httpException.StatusCode;
-                context.Response.ContentType = "application/json";
-                if (httpException != null)
-                {
-                    var bytes = Encoding.UTF8.GetBytes(httpException.Message);
-                    context.Response.Body = new MemoryStream(bytes);
-                }
-                string message = JsonConvert.SerializeObject(httpException, new JsonSerializerSettings()
-                {
-                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-                });
-                await context.Response.WriteAsync(message);
-            }
-        }
+	public async Task Invoke(HttpContext context) {
+	    try {
+		await this.next.Invoke(context);
+	    } catch (HttpException httpException) {
+		context.Response.StatusCode = httpException.StatusCode;
+		context.Response.ContentType = "application/json";
+		/*if (httpException != null) {
+		    var bytes = Encoding.UTF8.GetBytes(httpException.Message);
+		    context.Response.Body = new MemoryStream(bytes);
+		}*/
+
+		string message = JsonConvert.SerializeObject(httpException, options.Value.SerializerSettings);
+		await context.Response.WriteAsync(message);
+	    }
+	}
     }
 }
