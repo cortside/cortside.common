d30818dcb4e2f184ef3e8411627ad28480904700
Troy Horton <thorton@enerbankusa.com>
2020-03-25 12:40:06 -0600

    added receiverhostedservice

---
+++ src/Cortside.Common.DomainEvent/ReceiverHostedServiceSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cortside.Common.DomainEvent {
+    /// <summary>
+    /// Settings for the ReceiverHostedService
+    /// </summary>
+    public class ReceiverHostedServiceSettings {
+        /// <summary>
+        /// Message types for the Domain Event Receiver to handle
+        /// </summary>
+        public Dictionary<string, Type> MessageTypes { get; set; }
+
+        /// <summary>
+        /// Controls whether receiver hosted service is enabled
+        /// </summary>
+        public bool Disabled { get; set; }
+
+        /// <summary>
+        /// Frequency which the receiver attempts to connect to the message broker
+        /// </summary>
+        public double TimedInterval { get; set; }
+    }
+}
