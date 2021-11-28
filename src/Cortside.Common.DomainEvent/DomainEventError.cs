c8a6446cfc2c474261552eacf23ea4e2a0799963
Cort Schaefer <cort@eschaefer.net>
2021-11-28 12:09:45 -0700

    update to netstandard2.1 for all library projects, remove domainevent that was moved to it's own repository as well as remove other currently unused projects

--- src/Cortside.Common.DomainEvent/DomainEventError.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Cortside.Common.DomainEvent {
-    public class DomainEventError {
-        public string Condition { get; set; }
-        public string Description { get; set; }
-    }
-}
--- src/Cortside.Common.DomainEvent/DomainEventMessage.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Cortside.Common.DomainEvent {
-    public class DomainEventMessage<T> {
-        public string MessageId { get; set; }
-        public string CorrelationId { get; set; }
-        public T Data { get; set; }
-    }
-}
