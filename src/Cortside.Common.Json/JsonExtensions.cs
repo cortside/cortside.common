ebcad2f7aad7b804ea81b5cc955996222f4e6b1e
Cort Schaefer <cort@eschaefer.net>
2022-05-09 13:57:08 -0600

    add ToJson() extension method

---
+++ src/Cortside.Common.Json/JsonExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Cortside.Common.Json {
+    public static class JsonExtensions {
+        /// <summary>
+        /// The string representation of null.
+        /// </summary>
+        private static readonly string Null = "null";
+
+        /// <summary>
+        /// The string representation of exception.
+        /// </summary>
+        private static readonly string Exception = "Exception";
+
+        /// <summary>
+        /// To json.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The Json of any object.</returns>
+        public static string ToJson(this object value) {
+            if (value == null)
+                return Null;
+
+            string json = JsonConvert.SerializeObject(value);
+            return json;
+        }
+    }
+}
