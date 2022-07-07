13dcd0d4e494d6224ee53750aa2e8da616092dd9
Cort Schaefer <cort@eschaefer.net>
2022-07-07 11:19:00 -0600

    [ValidationListException] removed unneeded class

--- src/Cortside.Common.Messages/PropertyMessageException.cs
+++
@@ -1,7 +0,0 @@
-namespace Cortside.Common.Messages {
-    public class PropertyMessageException : MessageException {
-        public PropertyMessageException(string property) : base($"{property} is required.", property, $"{property} is required.") {
-            Property = property;
-        }
-    }
-}
