19ce7e8399844ade91f718f4fba47d024921f411
Cort Schaefer <cort@eschaefer.net>
2022-12-29 23:35:35 -0700

    update build and helper scripts

--- dependabot.ps1
+++ dependabot.ps1
@@ -1,6 +1,8 @@
-[cmdletBinding()]
-param(
-	[switch]$createpullrequest
+[CmdletBinding()]
+Param 
+(
+    [Parameter(Mandatory = $false)][string]$package = "",
+	[Parameter(Mandatory = $false)][switch]$createpullrequest
 )
 
 $ErrorActionPreference = "Stop"
@@ -45,42 +47,76 @@ git status
 Invoke-Exe git -args "checkout develop"
 Invoke-Exe git -args "pull"
 
-cp C:\work\cortside\cortside.templates\templates\api-editorconfig\src\.editorconfig .\src\.editorconfig
-cp C:\work\cortside\cortside.templates\templates\api-powershell\clean.ps1
-cp C:\work\cortside\cortside.templates\templates\api-powershell\update-nugetpackages.ps1
+cp C:\work\clean\cortside\cortside.templates\templates\api-editorconfig\src\.editorconfig .\src\.editorconfig
+cp C:\work\clean\cortside\cortside.templates\templates\api\clean.ps1
+cp C:\work\clean\cortside\cortside.templates\templates\api\update-nugetpackages.ps1
 
 if (Test-Path -path "cleanup.ps1") {
 	rm cleanup.ps1
 }
- 
-.\update-nugetpackages.ps1
+
+echo "prepping"
+
+.\clean.ps1 -quiet
 $result = check-result
 
-.\clean.ps1
+echo "about to restore"
+
+Invoke-Exe dotnet -args "restore src --verbosity quiet"
 $result = check-result
 
-#dotnet build src
-#$result = check-result
-
-dotnet test src
+echo "ready to update nuget packages"
+if ($package -eq "") { 
+	$body = (.\update-nugetpackages.ps1)
+} else {
+	$body = (Invoke-Exe dotnet -args "outdated src --include $package --pre-release Never --upgrade")
+}
+echo $body
 $result = check-result
 
-git status
+echo "checking to see if anything changed"
 
-$bot = "BOT-{0:yyyyMMdd}" -f (Get-Date)
-$branch = "feature/$bot"
+$files = (git status *.csproj | grep "modified:" | wc -l)
+if ($files -ne "0") {
+	dotnet test src
+	$result = check-result
 
-git add *.csproj
-git add clea*.ps1 
-git add update-nugetpackages.ps1 
-git add dependabot.ps1
-git add ./src/.editorconfig
-git status
-git checkout -b $branch
-git commit -m "[$branch] updated nuget packages"
-git push --set-upstream origin $branch
+	git status
 
-.\clean.ps1
-git checkout develop
+	$bot = "BOT-{0:yyyyMMdd}" -f (Get-Date)
+	$branch = "feature/$bot"
+	if ($package -ne "") {
+		$branch = "feature/$bot-$package"
+	}
 
-echo "should create the pr here -- everything passed - $branch"
+	git add *.csproj
+	git add clea*.ps1 
+	git add update-nugetpackages.ps1 
+	git add dependabot.ps1
+	git add git-pruneremote.ps1
+	git add ./src/.editorconfig
+	git status
+	git checkout -b $branch
+	if ($package -eq "") { 
+		git commit -m "[$branch] updated nuget packages"
+	} else {
+		git commit -m "[$branch] update $package"
+	}
+	git push --set-upstream origin $branch
+
+	$remote = (git remote -v)
+	if ($remote -like "*github.com*") {
+		gh repo set-default
+		gh pr create --title "$bot" --body "$body" --base develop
+	} else {
+		echo "should create the pr here -- everything passed - $branch"	
+		echo $body	
+	}
+
+	.\clean.ps1
+	git checkout develop
+
+	
+} else {
+	echo "no files changed"
+}
