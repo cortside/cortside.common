672447b51353d7e4f6e84e868e371c3ad503feb7
Cort Schaefer <cort@eschaefer.net>
2023-01-02 18:47:34 -0700

    update helper scripts; update nuget packages

--- clean.ps1
+++ clean.ps1
@@ -7,7 +7,7 @@ Function remove {
     param([string]$item)
     If (Test-Path $item){
 		if (-not $quiet.IsPresent) {
-			Write-Host "Removing $item"
+			Write-Output "Removing $item"
 		}
         Remove-Item $item -Force -Recurse
     }
@@ -15,13 +15,13 @@ Function remove {
 
 Function Invoke-Cleanup {
 	if (-not $quiet.IsPresent) {
-		Write-Host "---------------------"
-		Write-Host "Invoke-Cleanup"
-		Write-Host "---------------------"
+		Write-Output "---------------------"
+		Write-Output "Invoke-Cleanup"
+		Write-Output "---------------------"
 	}
 
 	# clean package, bin and obj folders
-	Get-ChildItem .\ -include packages,bin,obj,node_modules -Recurse | Where-Object {$_.FullName -NotMatch "BuildScripts"} | foreach ($_) { Write-Host "Removing " + $_.fullname; remove-item $_.fullname -Force -Recurse }
+	Get-ChildItem .\ -include packages,bin,obj,node_modules -Recurse | Where-Object {$_.FullName -NotMatch "BuildScripts"} | foreach ($_) { Write-Host "Removing $($_.fullname)"; remove-item $_.fullname -Force -Recurse }
 
 	#Find nunit files
 	Get-ChildItem -include *.nunit -Recurse |
@@ -30,7 +30,7 @@ Function Invoke-Cleanup {
 			Write-Host $_
 		}
 
-		$results = $_.BaseName + ".xml"
+		$results = "$($_.BaseName).xml"
 		If (Test-Path $results){
 				if (-not $quiet.IsPresent) {
 					Write-Host "Removing $results"
