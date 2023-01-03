2eac9c06ef4c034b411282e9fe9ecd58ab725beb
Cort Schaefer <cort@eschaefer.net>
2023-01-03 09:53:06 -0700

    minor cleanup

--- src/AssemblyVersionInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-[assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
-[assembly: AssemblyCopyrightAttribute("Copyright 2018 Cortside Consulting")]
-[assembly: AssemblyCompanyAttribute("Cortside Consulting")]
-[assembly: AssemblyProductAttribute("Cortside.Common")]
-
