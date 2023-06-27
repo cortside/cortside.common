aace252315ef87320186f8aa2331298383d83648
Cort Schaefer <cort@eschaefer.net>
2023-06-27 13:58:18 -0600

    [net6] cleanup from build lint analysis

--- src/Cortside.Common.Hosting.Tests/TimedHostedServiceTests.cs
+++ src/Cortside.Common.Hosting.Tests/TimedHostedServiceTests.cs
@@ -43,7 +43,9 @@ namespace Cortside.Common.Hosting.Tests {
             var stoppingToken = CancellationToken.None;
 
             // Act
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             instance.PublicExecuteAsync(stoppingToken);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             await Task.Delay(1000).ConfigureAwait(false);
 
             // Assert
