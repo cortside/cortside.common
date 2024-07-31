dc30266c4c1ad87920b4e19477260b86919dcd88
Cort Schaefer <cort@eschaefer.net>
2024-07-31 17:58:26 -0600

    update scripts from template

--- update-scripts.ps1
+++ update-scripts.ps1
@@ -7,3 +7,4 @@ cp ..\coeus\shoppingcart-api\generate-changelog.ps1
 cp ..\coeus\shoppingcart-api\update-nugetpackages.ps1
 cp ..\coeus\shoppingcart-api\update-targetframework.ps1
 cp ..\coeus\shoppingcart-api\src\.editorconfig .\src\.editorconfig
+cp ..\coeus\shoppingcart-api\Repository.psm1
