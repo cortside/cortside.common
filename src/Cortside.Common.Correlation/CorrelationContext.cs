3227a9b717b2207fe216fbd58cece0c4b10efbdc
Cort Schaefer <cort@eschaefer.net>
2024-08-01 20:10:44 -0600

    add CorrelationContext back and make generic without dependencies on aspnetcore

--- src/Cortside.Common.Correlation/CorrelationContext.cs
+++ src/Cortside.Common.Correlation/CorrelationContext.cs
@@ -1,43 +1,10 @@
 ﻿using System;
-using System.Linq;
 using System.Threading;
-using Microsoft.AspNetCore.Http;
 
 namespace Cortside.Common.Correlation {
     public static class CorrelationContext {
         private static readonly AsyncLocal<string> CorrelationId = new AsyncLocal<string>();
         private static readonly AsyncLocal<string> RequestId = new AsyncLocal<string>();
-
-        public static string SetFromHttpContext(HttpContext context) {
-            // get possible request headers for correlationId           
-            var xCorrelationId = GetHeaderValue(context, "X-Correlation-Id");
-            var requestId = GetHeaderValue(context, "Request-Id");
-
-            // set with most preferred header
-            string correlationId = xCorrelationId;
-
-            // set with requestId if it's not set already
-            correlationId ??= requestId;
-
-            // set with new guid if not set
-            correlationId ??= Guid.NewGuid().ToString();
-
-            // set values
-            SetCorrelationId(correlationId);
-            SetRequestId(context.TraceIdentifier);
-            return correlationId;
-        }
-
-        private static string GetHeaderValue(HttpContext context, string header) {
-            // check for correlationId as Request-Id from request
-            context.Request.Headers.TryGetValue(header, out var ids);
-            var id = ids.FirstOrDefault();
-            if (!string.IsNullOrWhiteSpace(id)) {
-                return id;
-            }
-
-            return null;
-        }
 
         public static void SetCorrelationId(string correlationId) {
             if (string.IsNullOrWhiteSpace(correlationId)) {
