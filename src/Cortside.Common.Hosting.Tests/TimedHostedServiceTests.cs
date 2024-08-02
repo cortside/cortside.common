ba00ca1e61badb766bbac0afdc505864ed61a003
Cort Schaefer <cort@eschaefer.net>
2024-08-01 18:35:02 -0600

    add TimedHostedService back

---
+++ src/Cortside.Common.Hosting.Tests/TimedHostedServiceTests.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Cortside.Common.Hosting.Tests {
+    public class TimedHostedServiceTests {
+        private class TestTimedHostedService : TimedHostedService {
+            public TestTimedHostedService(ILogger logger, bool enabled, int interval) : base(logger, enabled, interval) {
+            }
+
+            public Task PublicExecuteAsync(CancellationToken stoppingToken) {
+                return base.ExecuteAsync(stoppingToken);
+            }
+
+            protected override Task ExecuteIntervalAsync() {
+                Executed = true;
+                return Task.CompletedTask;
+            }
+
+            public bool Executed { get; set; } = false;
+        }
+
+        private readonly TestTimedHostedService instance;
+        private readonly Mock<ILogger> logger;
+
+        public TimedHostedServiceTests() {
+            logger = new Mock<ILogger>();
+            instance = new TestTimedHostedService(logger.Object, true, 500);
+        }
+
+        [Fact(Skip = "probably a good check to add")]
+        public void CannotConstructWithNullLogger() {
+            Assert.Throws<ArgumentNullException>(() => new TestTimedHostedService(default, true, 5000));
+        }
+
+        [Fact]
+        public async Task CanCallExecuteAsync() {
+            // Arrange
+            var stoppingToken = CancellationToken.None;
+
+            // Act
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            instance.PublicExecuteAsync(stoppingToken);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            await Task.Delay(1000);
+
+            // Assert
+            Assert.True(instance.Executed);
+        }
+    }
+}
---
+++ src/Cortside.Common.Hosting/TimedHostedService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Cortside.Common.Hosting {
+    /// <summary>
+    /// Base timed hosted service
+    /// </summary>
+    public abstract class TimedHostedService : BackgroundService {
+        protected readonly ILogger logger;
+        private readonly int interval;
+        private readonly bool enabled;
+
+        /// <summary>
+        /// Initializes new instance of the Hosted Service
+        /// </summary>
+        protected TimedHostedService(ILogger logger, bool enabled, int interval) {
+            this.logger = logger;
+            this.interval = interval;
+            this.enabled = enabled;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+            // force async so that hosted service does not block Startup
+#pragma warning disable _MissingConfigureAwait // Consider using .ConfigureAwait(false).
+            await Task.Yield();
+#pragma warning restore _MissingConfigureAwait // Consider using .ConfigureAwait(false).
+
+            if (enabled) {
+                logger.LogInformation($"{this.GetType().Name} is starting with interval of {interval} seconds");
+
+                stoppingToken.Register(() => logger.LogDebug($"{this.GetType().Name} is stopping."));
+
+                while (!stoppingToken.IsCancellationRequested) {
+                    await IntervalAsync();
+                    await Task.Delay(TimeSpan.FromSeconds(interval), stoppingToken).ConfigureAwait(false);
+                }
+                logger.LogInformation($"{this.GetType().Name} is stopping");
+            } else {
+                logger.LogInformation($"{this.GetType().Name} is disabled");
+            }
+        }
+
+        private async Task IntervalAsync() {
+            var correlationId = Guid.NewGuid().ToString();
+            using (logger.BeginScope(new Dictionary<string, object> { ["CorrelationId"] = correlationId })) {
+                logger.LogDebug($"{this.GetType().Name} is working");
+
+                try {
+                    await ExecuteIntervalAsync().ConfigureAwait(false);
+                } catch (Exception ex) {
+                    logger.LogError(ex, this.GetType().Name);
+                }
+            }
+        }
+
+        protected abstract Task ExecuteIntervalAsync();
+    }
+}
