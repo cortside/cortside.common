b00e3a48278574d4ff6e402dda23ce32e26d6e05
Erik <subreality68@gmail.com>
2024-09-27 09:53:52 -0600

    add preconditionfailed exception for filter

---
+++ src/Cortside.Common.Messages/MessageExceptions/PreconditionFailedResponseException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Cortside.Common.Messages.MessageExceptions {
+    [Serializable]
+    public class PreconditionFailedResponseException : MessageException {
+        public PreconditionFailedResponseException() : base() { }
+
+        public PreconditionFailedResponseException(string message) : base(message) { }
+
+        public PreconditionFailedResponseException(string message, Exception exception) : base(message, exception) { }
+
+        protected PreconditionFailedResponseException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        protected PreconditionFailedResponseException(string key, string property, params object[] properties) : base(key, property, properties) {
+        }
+
+        protected PreconditionFailedResponseException(string message, string property) : base(message, property) {
+        }
+    }
+}
